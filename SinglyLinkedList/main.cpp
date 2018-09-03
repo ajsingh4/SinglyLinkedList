7473a5227f08cf8d2a4ba9cc5c829c39a32fde98
Aj Gill <ajgill@ucdavis.edu>
2018-09-03 13:01:54 -0700

    Initial Commit

---
+++ SinglyLinkedList/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SinglyLinkedList
+//
+//  Created by Aj Gill on 9/3/18.
+//  Copyright © 2018 Aj Gill. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
